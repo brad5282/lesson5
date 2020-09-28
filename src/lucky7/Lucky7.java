ccaaf7c6f8cbb588e4eb63e80f30fe0d348266c3
brad5282 <brad5282@ISS2TNQKH2.scdsb.pri>
2020-09-28 12:38:15 -0400

    new

--- src/lucky7/Lucky7.java
+++ src/lucky7/Lucky7.java
@@ -1,12 +1,25 @@
-
 package lucky7;
 
+import java.util.Scanner;
 
 public class Lucky7 {
 
-    
     public static void main(String[] args) {
-        
+        Dice d = new Dice();
+        double money;
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.println("How much do you want to gamble today? ");
+            money = s.nextDouble();
+            if (money < 0) {
+                System.out.println("Invalid amount, we need to take something from you.");
+            } else {
+                break;
+            }
+
+        }
+        System.out.format("$%.2f, eh? Good luck.", money);
+
     }
-    
+
 }
